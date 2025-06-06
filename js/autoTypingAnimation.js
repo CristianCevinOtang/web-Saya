d5f204673182c122191b41c4af23e70c41242891
Cristian Celvin Otang <kk1842639@gmail.com>
2025-06-06 19:26:32 +0700

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,11 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span> Hi! I m a web designer and developer who, s passionate about creating clean, user-friendly websites. I love turning ideas into functional, beautiful digital experiences. Always learning, always building let s create something great together!"
+
+        
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
